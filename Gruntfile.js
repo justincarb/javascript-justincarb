48d6597c5b6aa515277c8de8814a15ff7bcef55e
direcode <direcode@gmail.com>
2015-05-18 17:22:59 -0500

    setting up grunt -- still not working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,23 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      css: {
-        files: [
-          '**/*.sass',
-          '**/*.scss'
-        ],
-        tasks: ['compass']
-      },
-      js: {
-        files: [
-          'js/*.js',
-          'Gruntfile.js'
-        ],
-        tasks: ['jshint']
-      }
-    },
-    compass: {
-      dist: {
-        options: {
-          sassDir: 'css/sass',
-          cssDir: 'css/css',
-          outputStyle: 'compressed'
-        }
-      }
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: ['Gruntfile.js', 'js/*.js']
-    }
-  });
-
-  // Load the Grunt plugins.
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-
-  // Register the default tasks.
-  grunt.registerTask('default', ['watch']);
-};
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'css/sass',
+          sassDir: 'css/scss',
+					cssDir: 'css/css'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['compass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
